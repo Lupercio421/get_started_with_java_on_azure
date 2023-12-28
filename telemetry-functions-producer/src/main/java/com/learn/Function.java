4fae52fb6b986432386071e5f23ecc354b50da63
Daniel L <daniel.llupercio@gmail.com>
2023-12-28 16:13:24 -0500

    function simulated a sensor that sends temp and pressure readings to event hub

--- telemetry-functions-producer/src/main/java/com/learn/Function.java
+++ telemetry-functions-producer/src/main/java/com/learn/Function.java
@@ -17,6 +17,22 @@ public class Function {
      * 2. curl "{your host}/api/HttpExample?name=HTTP%20Query"
      */
 
+    @FunctionName("generateSensorData")
+    @EventHubOutput(
+            name = "event",
+            eventHubName = "", // blank because the value is included in the connection string
+            connection = "EventHubConnectionString")
+    public TelemetryItem generateSensorData(
+            @TimerTrigger(
+                    name = "timerInfo",
+                    schedule = "*/10 * * * * *") // every 10 seconds
+            String timerInfo,
+            final ExecutionContext context) {
+        context.getLogger().info("Java Timer trigger function executed at: " + java.time.LocalDateTime.now());
+        double temperature = Math.random() * 100;
+        double pressure = Math.random() * 50;
+        return new TelemetryItem(temperature, pressure);
+    }
 
 //    @FunctionName("HttpExample")
 //    public HttpResponseMessage run(
